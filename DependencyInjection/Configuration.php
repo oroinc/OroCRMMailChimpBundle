d0eab144bd6e7bc10acc611707d5f0b5129a4442
Dmitry Khrysev <x86demon@gmail.com>
2014-09-10 14:34:41 +0300

    CRM-1952: Create OroCRM MailChimp Extension

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OroCRM\Bundle\MailChimpBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('orocrm_mailchimp');
+
+        return $treeBuilder;
+    }
+}
---
+++ OroCRMMailChimpBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OroCRM\Bundle\MailChimpBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OroCRMMailChimpBundle extends Bundle
+{
+}
