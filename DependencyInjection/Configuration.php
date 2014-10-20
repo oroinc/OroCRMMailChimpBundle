9cbe20e71fe3033da2ca35f386496771f475a2d5
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-10-20 18:49:58 +0300

    CRM-1962: Create import jobs (incl normalizers and dataconverters) for list and members - CR fixes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,9 +12,6 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('orocrm_mailchimp');
-
-        return $treeBuilder;
+        return new TreeBuilder();
     }
 }
