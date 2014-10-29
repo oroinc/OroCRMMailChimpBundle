6fa7b47d7a6408096f8002286aa1a33531f1e9ef
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-10-29 16:45:46 +0200

    CRM-2130: Job for synchronization of Segments and Marketing Lists (list adds and removals) - fix sync

--- Provider/Connector/StaticSegmentConnector.php
+++ Provider/Connector/StaticSegmentConnector.php
@@ -5,7 +5,9 @@ namespace OroCRM\Bundle\MailChimpBundle\Provider\Connector;
 use Oro\Bundle\IntegrationBundle\Provider\ConnectorInterface;
 use Oro\Bundle\IntegrationBundle\Provider\TwoWaySyncConnectorInterface;
 
-class StaticSegmentConnector extends AbstractMailChimpConnector implements TwoWaySyncConnectorInterface, ConnectorInterface
+class StaticSegmentConnector extends AbstractMailChimpConnector implements
+    TwoWaySyncConnectorInterface,
+    ConnectorInterface
 {
     const TYPE = 'staticSegment';
     const JOB_IMPORT = 'mailchimp_static_segment_import';
