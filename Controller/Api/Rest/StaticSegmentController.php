78adad5f746b38bd49017f9654498d048ec55074
Dmitry Khrysev <x86demon@gmail.com>
2014-11-06 17:03:30 +0200

    CRM-2136: Convert Marketing List connection from workflow to controllers

--- Controller/Api/Rest/StaticSegmentController.php
+++ Controller/Api/Rest/StaticSegmentController.php
@@ -2,13 +2,17 @@
 
 namespace OroCRM\Bundle\MailChimpBundle\Controller\Api\Rest;
 
-use Symfony\Component\HttpFoundation\Response;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\Rest\Util\Codes;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
-use Oro\Bundle\SoapBundle\Controller\Api\Rest\RestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
+
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Oro\Bundle\SoapBundle\Controller\Api\Rest\RestController;
+use OroCRM\Bundle\MailChimpBundle\Entity\StaticSegment;
 
 /**
  * @Rest\RouteResource("staticsegment")
@@ -32,6 +36,38 @@ class StaticSegmentController extends RestController implements ClassResourceInt
     public function deleteAction($id)
     {
         return $this->handleDeleteRequest($id);
+    }
+
+    /**
+     * @Rest\Post(
+     *      "/staticsegment/{id}/status",
+     *      requirements={"id"="\d+"}
+     * )
+     * @ParamConverter("staticSegment", options={"id"="id"})
+     * @Rest\QueryParam(
+     *      name="id",
+     *      requirements="\d+",
+     *      nullable=false,
+     *      description="Static Segment Id"
+     * )
+     * @ApiDoc(
+     *      description="Update Static Segment status",
+     *      resource=false
+     * )
+     * @AclAncestor("orocrm_marketing_list_edit")
+     * @param StaticSegment $staticSegment
+     * @return Response
+     */
+    public function updateStatusAction(StaticSegment $staticSegment)
+    {
+        $status = $this->getRequest()->get('status');
+        $staticSegment->setSyncStatus($status);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($staticSegment);
+        $em->flush();
+
+        return $this->handleView($this->view('', Codes::HTTP_OK));
     }
 
     /**
