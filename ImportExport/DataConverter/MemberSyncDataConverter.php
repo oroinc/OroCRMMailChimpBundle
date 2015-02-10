343f1e533c0b3ffdfe9cac632f0d1ce4ecee5213
Alexander Kuzmenko <akuzmenko@magecore.com>
2015-02-10 12:54:45 +0200

    CRM-2441: MailChimp integration failed with more than 50 members  - fixed issue with last batch not being processed

--- ImportExport/DataConverter/MemberSyncDataConverter.php
+++ ImportExport/DataConverter/MemberSyncDataConverter.php
@@ -2,14 +2,24 @@
 
 namespace OroCRM\Bundle\MailChimpBundle\ImportExport\DataConverter;
 
-use Oro\Bundle\LocaleBundle\Model\FirstNameInterface;
-use Oro\Bundle\LocaleBundle\Model\LastNameInterface;
 use OroCRM\Bundle\MailChimpBundle\Entity\Member;
 use OroCRM\Bundle\MailChimpBundle\Model\MergeVar\MergeVarInterface;
 use OroCRM\Bundle\MarketingListBundle\Provider\ContactInformationFieldsProvider;
 
 class MemberSyncDataConverter extends MemberDataConverter
 {
+    /** @var array */
+    protected $namePartsMap = [
+        MergeVarInterface::TAG_FIRST_NAME  => [
+            'interface'          => 'Oro\\Bundle\\LocaleBundle\\Model\\FirstNameInterface',
+            'suggestedFieldName' => 'firstName'
+        ],
+        MergeVarInterface::TAG_LAST_NAME   => [
+            'interface'          => 'Oro\\Bundle\\LocaleBundle\\Model\\LastNameInterface',
+            'suggestedFieldName' => 'lastName'
+        ]
+    ];
+
     /**
      * @var ContactInformationFieldsProvider
      */
@@ -29,18 +39,27 @@ class MemberSyncDataConverter extends MemberDataConverter
     public function convertToImportFormat(array $importedRecord, $skipNullValues = true)
     {
         $marketingListData = reset($importedRecord);
-        $item = [
-            MergeVarInterface::FIELD_TYPE_EMAIL => $marketingListData['email'],
-            MergeVarInterface::TAG_EMAIL => $marketingListData['email'],
-            'status' => Member::STATUS_EXPORT,
-        ];
+        $entityClassName   = !empty($importedRecord['entityClass']) ? $importedRecord['entityClass'] : null;
 
-        if (isset($marketingListData['firstName'])) {
-            $item[MergeVarInterface::TAG_FIRST_NAME] = $marketingListData['firstName'];
+        if ($entityClassName) {
+            $contactFieldsValues = $this->getContactInformationFieldsValues($entityClassName, $marketingListData);
+        } else {
+            $contactFieldsValues = [$marketingListData['email']];
         }
 
-        if (isset($marketingListData['lastName'])) {
-            $item[MergeVarInterface::TAG_LAST_NAME] = $marketingListData['lastName'];
+        $item = [
+            MergeVarInterface::FIELD_TYPE_EMAIL => reset($contactFieldsValues),
+            MergeVarInterface::TAG_EMAIL        => reset($contactFieldsValues),
+            'status'                            => Member::STATUS_EXPORT,
+        ];
+
+        if ($entityClassName) {
+            foreach ($this->namePartsMap as $part => $metadata) {
+                if (in_array($metadata['interface'], class_implements($entityClassName))
+                    && !empty($marketingListData[$metadata['suggestedFieldName']])) {
+                    $item[$part] = $marketingListData[$metadata['suggestedFieldName']];
+                }
+            }
         }
 
         if (!empty($importedRecord['subscribersList_id'])) {
@@ -55,19 +74,20 @@ class MemberSyncDataConverter extends MemberDataConverter
     }
 
     /**
-     * @param object $object
+     * @param string $entityClassName
+     * @param array  $data
      * @return array
      */
-    protected function getContactInformationFieldsValues($object)
+    protected function getContactInformationFieldsValues($entityClassName, $data)
     {
         $contactInformationFields = $this->contactInformationFieldsProvider->getEntityTypedFields(
-            $object,
+            $entityClassName,
             ContactInformationFieldsProvider::CONTACT_INFORMATION_SCOPE_EMAIL
         );
 
         $values = $this->contactInformationFieldsProvider->getTypedFieldsValues(
             $contactInformationFields,
-            $object
+            $data
         );
 
         return array_filter($values);
