0e9743bc490569febadfcba8c7ea0d64b35b09e6
Dmitry Khrysev <x86demon@gmail.com>
2016-01-20 17:23:42 +0200

    CRM-4359: [Hours] Members don't imported to Mailchimp after changing List without Disconnect - fixed lists duplication on name change - updated test coverage

--- Validator/UniqueStaticSegmentNameValidator.php
+++ Validator/UniqueStaticSegmentNameValidator.php
@@ -36,7 +36,7 @@ class UniqueStaticSegmentNameValidator extends ConstraintValidator
 
             $segments = $this->transport->getListStaticSegments($value->getSubscribersList());
             foreach ($segments as $segment) {
-                if ($segment['name'] == $value->getName()) {
+                if ($segment['name'] === $value->getName()) {
                     $this->context->addViolationAt('name', $constraint->message);
                     break;
                 }
