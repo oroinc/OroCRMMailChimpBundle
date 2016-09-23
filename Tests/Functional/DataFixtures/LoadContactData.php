cb341614f2d3ff2153e256112fbc54dc398f5d15
Makar Sichevoy <sichevoy@gmail.com>
2016-09-23 10:06:10 +0300

    BAP-11808: Move CRM bundles to Oro namespace

--- Tests/Functional/DataFixtures/LoadContactData.php
+++ Tests/Functional/DataFixtures/LoadContactData.php
@@ -5,8 +5,8 @@ namespace OroCRM\Bundle\MailChimpBundle\Tests\Functional\DataFixtures;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-use OroCRM\Bundle\ContactBundle\Entity\Contact;
-use OroCRM\Bundle\ContactBundle\Entity\ContactEmail;
+use Oro\Bundle\ContactBundle\Entity\Contact;
+use Oro\Bundle\ContactBundle\Entity\ContactEmail;
 
 class LoadContactData extends AbstractFixture
 {
