02268fbfd7fd2ddbd4493f3944363ee87bb3a6d9
Dmitry Khrysev <x86demon@users.noreply.github.com>
2017-03-14 23:29:55 +0200

    CRM-7926: Two or more MailChimp integrations are not together - reverted BC break changes, made fix without BC and requirement to update platform (#8498)

--- ImportExport/Step/StepExecutor.php
+++ ImportExport/Step/StepExecutor.php
@@ -3,20 +3,14 @@
 namespace OroCRM\Bundle\MailChimpBundle\ImportExport\Step;
 
 use Akeneo\Bundle\BatchBundle\Item\InvalidItemException;
-use Akeneo\Bundle\BatchBundle\Item\ItemReaderInterface;
 
 use Oro\Bundle\BatchBundle\Step\StepExecutionWarningHandlerInterface;
 use Oro\Bundle\BatchBundle\Step\StepExecutor as BaseStepExecutor;
-use Oro\Bundle\ImportExportBundle\Reader\IteratorBasedReader;
+use OroCRM\Bundle\MailChimpBundle\ImportExport\Reader\AbstractIteratorBasedReader;
 use OroCRM\Bundle\MailChimpBundle\ImportExport\Reader\SubordinateReaderInterface;
 
 class StepExecutor extends BaseStepExecutor
 {
-    /**
-     * @var ItemReaderInterface|IteratorBasedReader
-     */
-    protected $reader;
-
     /**
      * {@inheritdoc}
      */
@@ -52,7 +46,7 @@ class StepExecutor extends BaseStepExecutor
                     }
                 }
 
-                if ($this->reader instanceof IteratorBasedReader) {
+                if ($this->reader instanceof AbstractIteratorBasedReader) {
                     $sourceIterator = $this->reader->getSourceIterator();
                     if ($sourceIterator instanceof SubordinateReaderInterface && $sourceIterator->writeRequired()) {
                         $scheduleWrite = true;
