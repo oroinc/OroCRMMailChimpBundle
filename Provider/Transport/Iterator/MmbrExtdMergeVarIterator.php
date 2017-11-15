7555aa510c378ca5acde2a860d5845d701e19f5c
Yefim Yevtushenko <yyevtushenko@oroinc.com>
2017-11-15 17:46:52 +0200

    CRM-8623: MailChimp rejected subscribers trigger ERRORs - fixed do not create MemberExtendedMergeVar for members with status export_failed - mark MemberExtendedMergeVar as dropped for members which are not present in mailchimp list
    Conflicts:
    	package/mailchimp/Provider/Transport/Iterator/MmbrExtdMergeVarIterator.php

--- Provider/Transport/Iterator/MmbrExtdMergeVarIterator.php
+++ Provider/Transport/Iterator/MmbrExtdMergeVarIterator.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\AbstractQuery;
 
 use Oro\Bundle\BatchBundle\ORM\Query\BufferedQueryResultIterator;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\MailChimpBundle\Entity\Member;
 use Oro\Bundle\MailChimpBundle\Model\ExtendedMergeVar\ProviderInterface;
 use Oro\Bundle\MarketingListBundle\Provider\MarketingListProvider;
 use Oro\Bundle\MailChimpBundle\Entity\StaticSegment;
@@ -79,6 +80,7 @@ class MmbrExtdMergeVarIterator extends AbstractStaticSegmentMembersIterator
 
         $marketingList = $staticSegment->getMarketingList();
         $memberIdentifier = self::MEMBER_ALIAS . '.id';
+        $memberStatus = self::MEMBER_ALIAS . '.status';
         $fieldExpr = $this->fieldHelper
             ->getFieldExpr(
                 $marketingList->getEntity(),
@@ -90,9 +92,12 @@ class MmbrExtdMergeVarIterator extends AbstractStaticSegmentMembersIterator
 
         $qb->andWhere(
             $qb->expr()->andX(
-                $qb->expr()->isNotNull($memberIdentifier)
+                $qb->expr()->isNotNull($memberIdentifier),
+                $qb->expr()->neq($memberStatus, ':status_export_failed')
             )
         );
+
+        $qb->setParameter('status_export_failed', Member::STATUS_EXPORT_FAILED);
 
         $bufferedIterator = new BufferedQueryResultIterator($qb);
         $bufferedIterator->setHydrationMode(AbstractQuery::HYDRATE_ARRAY)->setReverse(true);
