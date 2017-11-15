f2842a0f17fbff1c58f8584906595086d8f7b87d
Serj Bielanovskyi <sbielanovskyi@oroinc.com>
2017-11-15 20:09:34 +0200

    BAP-15981: Unexpected staled jobs - set 1h time_before_stale for oro_mailchimp:export_mailchimp job

--- DependencyInjection/OroMailChimpExtension.php
+++ DependencyInjection/OroMailChimpExtension.php
@@ -2,12 +2,14 @@
 
 namespace Oro\Bundle\MailChimpBundle\DependencyInjection;
 
+use Oro\Bundle\MailChimpBundle\Async\ExportMailChimpProcessor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class OroMailChimpExtension extends Extension
+class OroMailChimpExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -16,5 +18,19 @@ class OroMailChimpExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $messageQueueConfigs = $container->getExtensionConfig('oro_message_queue');
+        foreach ($messageQueueConfigs as $messageQueueConfig) {
+            if (isset($messageQueueConfig['time_before_stale'])
+                && array_key_exists('jobs', $messageQueueConfig['time_before_stale'])
+            ) {
+                $config['time_before_stale']['jobs'][ExportMailChimpProcessor::JOB_NAME_PREFIX]
+                    = ExportMailChimpProcessor::JOB_TIME_BEFORE_STALE;
+                $container->prependExtensionConfig('oro_message_queue', $config);
+            }
+        }
     }
 }
