f2d832fb7104f86b026a86c9840ce7a05e18dfcd
Yefim Yevtushenko <webevt@users.noreply.github.com>
2017-11-27 11:59:20 +0200

    CRM-8657: Optimize Marketing Lists updates and MailChimp export job (#15077)
    * CRM-8657: Optimize Marketing Lists updates and MailChimp export job
    - added STATUS_SCHEDULED_BY_CHANGE sync status
    - made NotifyMailChimpListener set scheduled_by_change sync status to StaticSegment
    - added conditions to schedule_mailchimp_segment_synchronization process
    - fixed tests for ExportMailChimpProcessor
    - added oro_mailchimp.static_segment_sync_mode configuration
    - added form type StaticSegmentSyncModeType, choices provider StaticSegmentSyncModeChoicesProvider
    - added unit tests
    
    * CRM-8657: Optimize Marketing Lists updates and MailChimp export job
    - removed StaticSegmentSyncModeChoicesProviderInterface interface
    - removed redundant argument in StaticSegmentSyncModeChoicesProvider
    - made use of constants in schedule_mailchimp_segment_synchronization process
    - updated unit test

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,16 +2,30 @@
 
 namespace Oro\Bundle\MailChimpBundle\DependencyInjection;
 
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
+    const STATIC_SEGMENT_SYNC_MODE_ON_UPDATE = 'on_update';
+    const STATIC_SEGMENT_SYNC_MODE_SCHEDULED = 'scheduled';
+
     /**
      * {@inheritdoc}
      */
     public function getConfigTreeBuilder()
     {
-        return new TreeBuilder();
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('oro_mailchimp');
+
+        SettingsBuilder::append(
+            $rootNode,
+            [
+                'static_segment_sync_mode' => ['value' => self::STATIC_SEGMENT_SYNC_MODE_ON_UPDATE],
+            ]
+        );
+
+        return $treeBuilder;
     }
 }
---
+++ Tests/Unit/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Oro\Bundle\MailChimpBundle\Tests\Unit\DependencyInjection;
+
+use Oro\Bundle\MailChimpBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetConfigTreeBuilder()
+    {
+        $configuration = new Configuration();
+
+        $this->assertInstanceOf(
+            TreeBuilder::class,
+            $configuration->getConfigTreeBuilder()
+        );
+    }
+
+    /**
+     * @dataProvider processConfigurationDataProvider
+     *
+     * @param array $configs
+     * @param array $expected
+     */
+    public function testProcessConfiguration(array $configs, array $expected)
+    {
+        $configuration = new Configuration();
+        $processor = new Processor();
+        $this->assertEquals($expected, $processor->processConfiguration($configuration, $configs));
+    }
+
+    /**
+     * @return array
+     */
+    public function processConfigurationDataProvider()
+    {
+        return [
+            'empty' => [
+                'configs' => [[]],
+                'expected' => [
+                    'settings' => [
+                        'resolved' => 1,
+                        'static_segment_sync_mode' => [
+                            'value' => 'on_update',
+                            'scope' => 'app',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
